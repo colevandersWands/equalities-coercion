f06162017fc0fc4d6e7f2acdc2e708cc68ae11d7
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2019-10-01 16:13:36 +0200

    tidy

--- tabling/cast.js
+++ tabling/cast.js
@@ -1,32 +1,40 @@
 function cast(type, value, id) {
 
-    if (type == "number" || type == "Number") {
-        return Number(value);
+  if (type == "number" || type == "Number") {
+    return Number(value);
 
-    } else if (type == "string" || type == "String") {
-        return value;
+  } else if (type == "string" || type == "String") {
+    return value;
 
-    } else if (type == "null" || type == "Null") {
-        return null;
+  } else if (type == "null" || type == "Null") {
+    if (value !== "null"
+      || value !== "Null"
+      || value !== "NULL") {
+      let field = document.getElementById(id);
+      field.style.backgroundColor = 'purple';
+      throw new TypeError(value + ' is not null');
+    }
 
-    } else if (type == "boolean" || type == "Boolean") {
-        if (value === "true") {
-            return true;
-        } else if(value === 'false') {
-            return false;
-        } else {   
-            try { 
-                var field = document.getElementById(id);
-                field.style.backgroundColor = 'yellow';
-                throw new Error(value + ' is not boolean');
-            } catch(err) {
-                throw new Error(value + ' is not boolean');
-            };
-        };
+    return null;
+
+  } else if (type == "boolean" || type == "Boolean") {
+    if (value === "true") {
+      return true;
+    } else if (value === 'false') {
+      return false;
+    } else {
+      try {
+        let field = document.getElementById(id);
+        field.style.backgroundColor = 'purple';
+        throw new TypeError(value + ' is not boolean');
+      } catch (err) {
+        throw new TypeError(value + ' is not boolean');
+      };
     };
+  };
 
-    // functions return undefined by default
-    //  so if they asked for undefined, they'll get it
-    //  and if they ask for an invalid type, they'll get undefined
+  // functions return undefined by default
+  //  so if they asked for undefined, they'll get it
+  //  and if they ask for an invalid type, they'll get undefined
 
 };
